6225ceaf84ebb65c1449933f562fe8b657697574
Jenelyn Castil Contillo <jenelyncontillo02141995@gmail.com>
2024-01-15 15:58:08 +0800

    Wise lending APY

--- src/adaptors/wise-lending/index.js
+++ src/adaptors/wise-lending/index.js
@@ -1,18 +1,18 @@
 const sdk = require('@defillama/sdk3');
+const { ethers } = require('ethers');
+const Web3 = require('web3');
+const wiseLendingABI = require('./abi/wiseLendingABI.json');
+const wiseSecurityABI = require('./abi/wiseSecurityABI.json');
+const erc20ABI = require('./abi/erc20ABI.json');
+const { getProvider } = require('@defillama/sdk/build/general');
 const superagent = require('superagent');
-const BigNumber = require('bignumber.js');
 
-const AddressMap = {
-  ethereum: {
-    WETH: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
-    UiPoolDataProvider: '0x5250cCE48E43AB930e45Cc8E71C87Ca4B51244cf',
-    LendPoolAddressProvider: '0x24451F47CaF13B24f4b5034e1dF6c0E401ec0e46',
-    Bend: '0x0d02755a5700414B26FF040e1dE35D337DF56218',
-    BendProtocolIncentivesController:
-      '0x26FC1f11E612366d3367fc0cbFfF9e819da91C8d',
-    UniswapV2PairWETH: '0x336ef4e633b1117dca08c1a57f4139c62c32c935',
-    StakedBUNI: '0x647C509AF2A2b2294bB79fCE12DaEc8e7cf938f7',
-  },
+const WISE_LENDING_CONTRACT = '0x37e49bf3749513A02FA535F0CbC383796E8107E4';
+const WISE_SECURITY_CONTRACT = '0x829c3AE2e82760eCEaD0F384918a650F8a31Ba18';
+const FEE_MANAGER_CONTRACT = '0x0bC24E61DAAd6293A1b3b53a7D01086BfF0Ea6e5';
+
+const address = {
+  WETH: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
 };
 
 const ChainName = {
@@ -20,187 +20,73 @@ const ChainName = {
 };
 
 const projectSlug = 'wise-lending';
+const tokens = [
+  'WETH'
+];
+
+const web3 = new Web3('https://mainnet.infura.io/v3/b2ca877d39fa4a1c99e9120a03d53e57');
 
 async function apy() {
   const pools = await Promise.all(
     ['ethereum'].map(async (chain) => {
-      const [prices, { output: reserveList }] = await Promise.all([
-        (async () => {
-          const coins = [
-            `${chain}:${AddressMap[chain].WETH}`,
-            `${chain}:${AddressMap[chain].Bend}`,
-          ]
-            .join(',')
-            .toLowerCase();
-
-          const ret = await superagent.get(
-            `https://coins.llama.fi/prices/current/${coins}`
-          );
-          return ret.body.coins;
-        })(),
-        sdk.api.abi.call({
-          target: AddressMap[chain].UiPoolDataProvider,
-          abi: 'function getSimpleReservesData(address provider) view returns (tuple(address underlyingAsset, string name, string symbol, uint256 decimals, uint256 reserveFactor, bool borrowingEnabled, bool isActive, bool isFrozen, uint128 liquidityIndex, uint128 variableBorrowIndex, uint128 liquidityRate, uint128 variableBorrowRate, uint40 lastUpdateTimestamp, address bTokenAddress, address debtTokenAddress, address interestRateAddress, uint256 availableLiquidity, uint256 totalVariableDebt, uint256 priceInEth, uint256 variableRateSlope1, uint256 variableRateSlope2)[])',
-          params: [AddressMap[chain].LendPoolAddressProvider],
-          chain,
-        }),
-      ]);
-
-      const ethPriceUsd =
-        prices[`${chain}:${AddressMap[chain].WETH}`.toLowerCase()].price;
-      const bendPriceUsd =
-        prices[`${chain}:${AddressMap[chain].Bend}`.toLowerCase()].price;
-
       return (
         await Promise.all(
           // Lend Pool
           [
             Promise.all(
-              reserveList.map(async (reserve) => {
-                const [
-                  {
-                    output: [
-                      { output: bTokenAsset },
-                      { output: debtTokenAsset },
-                    ],
-                  },
+              tokens.map(async (token) => {
 
-                  {
-                    output: [
-                      { output: bTokenTotalSupply },
-                      { output: debtTokenTotalSupply },
-                    ],
-                  },
-                ] = await Promise.all([
-                  sdk.api.abi.multiCall({
-                    target: AddressMap[chain].BendProtocolIncentivesController,
-                    abi: 'function assets(address) view returns (uint128 emissionPerSecond, uint128 lastUpdateTimestamp, uint256 index)',
-                    calls: [
-                      { params: [reserve.bTokenAddress] },
-                      { params: [reserve.debtTokenAddress] },
-                    ],
-                    chain,
-                  }),
-                  sdk.api.abi.multiCall({
-                    abi: 'erc20:totalSupply',
-                    calls: [
-                      { target: reserve.bTokenAddress },
-                      { target: reserve.debtTokenAddress },
-                    ],
-                  }),
-                ]);
+                const tokenAddress = `${chain}:${address[token]}`;
 
-                const usdPrice = new BigNumber(reserve.priceInEth)
-                  .multipliedBy(ethPriceUsd)
-                  .shiftedBy(-18)
-                  .shiftedBy(0 - reserve.decimals);
+                const usdPrice = (
+                  await superagent.get(
+                    `https://coins.llama.fi/prices/current/${tokenAddress}`
+                  )
+                ).body.coins[tokenAddress];
 
-                const availableLiquidityUsd = new BigNumber(
-                  reserve.availableLiquidity
-                ).multipliedBy(usdPrice);
-                const totalVariableDebtUsd = new BigNumber(
-                  reserve.totalVariableDebt
-                ).multipliedBy(usdPrice);
+                const wiseSecurityContract = await new web3.eth.Contract(
+                  wiseSecurityABI,
+                  WISE_SECURITY_CONTRACT
+                );
+
+                const lendingRate = await wiseSecurityContract.methods.getLendingRate(address[token]).call() / 1e16;
+                const borrowRate = await wiseSecurityContract.methods.getBorrowRate(address[token]).call() / 1e16;
+
+                const wiseLendingContract = await new web3.eth.Contract(
+                  wiseLendingABI,
+                  WISE_LENDING_CONTRACT
+                );
+
+                const totalBorrow = await wiseLendingContract.methods.getPseudoTotalBorrowAmount(address[token]).call() / 1e18;
+                const totalSupply = await wiseLendingContract.methods.getPseudoTotalPool(address[token]).call() / 1e18;
+
+                const tokenContract = await new web3.eth.Contract(
+                  erc20ABI,
+                  address[token]
+                );
+
+                const balance = await tokenContract.methods.balanceOf(WISE_LENDING_CONTRACT).call() / 1e18;
+
+                const tvlUsd = balance * usdPrice.price;
+                const totalSupplyUsd = totalSupply * usdPrice.price;
+                const totalBorrowUsd = totalBorrow * usdPrice.price;
 
                 return {
-                  pool: `${reserve.bTokenAddress}-${chain}`,
+                  pool: `${address[token]}-${chain}`,
                   chain: ChainName[chain],
                   project: projectSlug,
-                  symbol: reserve.symbol,
-                  tvlUsd: availableLiquidityUsd.toNumber(),
-                  apyBase: new BigNumber(reserve.liquidityRate)
-                    .shiftedBy(-27)
-                    .multipliedBy(100)
-                    .toNumber(),
-                  apyReward: new BigNumber(bTokenAsset.emissionPerSecond)
-                    .multipliedBy(365 * 24 * 60 * 60)
-                    .multipliedBy(bendPriceUsd)
-                    .dividedBy(bTokenTotalSupply)
-                    .dividedBy(ethPriceUsd)
-                    .multipliedBy(100)
-                    .toNumber(),
-                  rewardTokens: [AddressMap[chain].Bend],
-                  underlyingTokens: [reserve.underlyingAsset],
-                  url: 'https://benddao.xyz',
-                  apyBaseBorrow: new BigNumber(reserve.variableBorrowRate)
-                    .shiftedBy(-27)
-                    .multipliedBy(100)
-                    .toNumber(),
-                  apyRewardBorrow: new BigNumber(
-                    debtTokenAsset.emissionPerSecond
-                  )
-                    .multipliedBy(365 * 24 * 60 * 60)
-                    .multipliedBy(bendPriceUsd)
-                    .dividedBy(debtTokenTotalSupply)
-                    .dividedBy(ethPriceUsd)
-                    .multipliedBy(100)
-                    .toNumber(),
-                  totalSupplyUsd: availableLiquidityUsd
-                    .plus(totalVariableDebtUsd)
-                    .toNumber(),
-                  totalBorrowUsd: totalVariableDebtUsd.toNumber(),
-                  borrowable: reserve.borrowingEnabled,
+                  symbol: usdPrice.symbol,
+                  tvlUsd,
+                  apyBase: lendingRate,
+                  // apyReward: lendingRate,
+                  url: 'https://wiselending.com/',
+                  apyBaseBorrow: borrowRate,
+                  // apyRewardBorrow: borrowRate,
+                  totalSupplyUsd,
+                  totalBorrowUsd,
                 };
               })
             ),
-
-            // Liquidity Pool
-            (async () => {
-              const [
-                { output: apr },
-                {
-                  output: [{ output: wethBalance }, { output: bendBalance }],
-                },
-              ] = await Promise.all([
-                sdk.api.abi.call({
-                  target: AddressMap[chain].StakedBUNI,
-                  abi: 'uint256:apr',
-                  chain,
-                }),
-                sdk.api.abi.multiCall({
-                  abi: 'erc20:balanceOf',
-                  chain,
-                  calls: [
-                    {
-                      target: AddressMap[chain].WETH,
-                      params: [AddressMap[chain].UniswapV2PairWETH],
-                    },
-                    {
-                      target: AddressMap[chain].Bend,
-                      params: [AddressMap[chain].UniswapV2PairWETH],
-                    },
-                  ],
-                }),
-              ]);
-
-              return {
-                pool: `${AddressMap[chain].UniswapV2PairWETH}-${chain}`,
-                chain: ChainName[chain],
-                project: projectSlug,
-                symbol: 'BEND-WETH',
-                tvlUsd: new BigNumber(
-                  new BigNumber(wethBalance)
-                    .shiftedBy(-18)
-                    .multipliedBy(ethPriceUsd)
-                )
-                  .plus(
-                    new BigNumber(bendBalance)
-                      .shiftedBy(-18)
-                      .multipliedBy(bendPriceUsd)
-                  )
-                  .toNumber(),
-                apyBase: new BigNumber(apr)
-                  .shiftedBy(-18)
-                  .multipliedBy(100)
-                  .toNumber(),
-                rewardTokens: [AddressMap[chain].Bend],
-                underlyingTokens: [
-                  AddressMap[chain].Bend,
-                  AddressMap[chain].WETH,
-                ],
-                url: 'https://benddao.xyz',
-              };
-            })(),
           ]
         )
       ).flat();
